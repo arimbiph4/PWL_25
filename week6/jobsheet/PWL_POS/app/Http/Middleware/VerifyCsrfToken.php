558143da3268e5e31ba34a33b6beab5a8f756419
arimbiph4 <andikaputra022005@gmail.com>
2025-03-24 15:05:24 +0700

    week6

---
+++ week6/jobsheet/PWL_POS/app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        'level/list', // Tambahkan rute ini agar tidak diperiksa CSRF
+    ];
+    
+}
